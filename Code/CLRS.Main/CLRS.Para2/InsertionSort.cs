ddcb8a36047a5c6c3e93c5c0afa0eb92e884cfa0
winwink <215917264@qq.com>
2015-01-18 20:58:52 +0800

    para2

---
+++ Code/CLRS.Main/CLRS.Para2/InsertionSort.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+
+namespace CLRS.Para2
+{
+    class InsertionSort
+    {
+        public int[] GetAnInt(int length)
+        {
+            if (length <= 0) throw new  ArgumentException("length must lager than 0");
+            int[] a = new int[length];
+            Random random = new Random();
+            for(int i=0;i<length;i++)
+            {
+                a[i] = random.Next(0, 100);
+            }
+            return a;
+        }
+
+        public void SortUnDesc(int[] a)
+        {
+            for (int j = 1; j < a.Length; j++)
+            {
+                int key = a[j];
+                int i = j - 1;
+                while (i >= 0 && a[i] > key)
+                {
+                    a[i + 1] = a[i];
+                    i --;
+                }
+                a[i + 1] = key;
+            }
+        }
+        //question 2.1-2
+        public void SortUnAsc(int[] a)
+        {
+            for (int j = 1; j < a.Length; j++)
+            {
+                int key = a[j];
+                int i = j - 1;
+                while (i >= 0 && a[i] < key)
+                {
+                    a[i + 1] = a[i];
+                    i--;
+                }
+                a[i + 1] = key;
+            }
+        }
+
+        public void FindNum(int[] a, int v)
+        {
+            
+        }
+    }
+}
---
+++ Code/CLRS.Main/CLRS.Para2/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CLRS.Para2
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+
+        public void Test1()
+        {
+            InsertionSort ins =new InsertionSort();
+            var a = ins.GetAnInt(10);
+            var list = a.ToList();
+            Console.WriteLine("start");
+            list.ForEach(m=>Console.Write(m+" "));
+            Console.WriteLine();
+
+            Console.WriteLine("sort un desc");
+            int[] b = new int[a.Length];
+            a.CopyTo(b,0);
+            ins.SortUnDesc(b);
+            var list2 = b.ToList();
+            list2.ForEach(m=>Console.Write(m+" "));
+            Console.WriteLine();
+
+            Console.WriteLine("sort un asc");
+            int[] c=new int[a.Length];
+            a.CopyTo(c,0);
+            ins.SortUnAsc(c);
+            var list3 = c.ToList();
+            list3.ForEach(m=>Console.Write(m+" "));
+
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            Test1();
+        }
+    }
+}
